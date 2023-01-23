c1b68a2579fc28177b40dd1f15877191d7a1b221
Daniel Joanol <d.joanol@cleverpy.com>
2023-01-23 17:57:22 +0100

    Updates email message

--- src/main/java/com/danieljoanol/forms/constants/Email.java
+++ src/main/java/com/danieljoanol/forms/constants/Email.java
@@ -3,15 +3,20 @@ package com.danieljoanol.forms.constants;
 public class Email {
     
     //TODO: change company name
-    public static final String SIGNATURE = "Best regards, \nWorkshop Forms team";
+    public static final String SIGNATURE = "Un saludo cordial,<br>Equipo de Workshop Forms";
     public static final String CODE_TITLE = "Nuevo código de confirmación";
     
     private Email() {}
 
     public static String codeMessage(String firstName, int code, int timeLimit) {
-        return "Hola " + firstName + ", \n\nEl código de confirmación és " + code + ". \n" + 
-                "Inserte el código en la pantalla de confirmación para confirmar la operación." +
-                "\nEl código es válido por " + timeLimit+ " minutos." +
-                "\n\n" + SIGNATURE;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hola " + firstName + ",<br><br>");
+        sb.append("El código de confirmación és " + code + ".<br>");
+        sb.append("Inserta el código en la pantalla de confirmación para confirmar la operación.<br>");
+        sb.append("Acuerdate que el código es válido por " + timeLimit+ " minutos.<br><br>");
+        sb.append(SIGNATURE);
+        
+        return sb.toString();
     }
 }
